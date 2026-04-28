b24dd96c72d28738520acd3cf9ae0f66d400166c
Yan Henrry <yan.zhenrry@gmail.com>
2026-04-28 20:25:56 -0300

    Firebase conected

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 import 'app.dart';
 
 const firebaseConfig = FirebaseOptions(
-  apiKey: "AIzaSyCoQ4CEYaAdaA5VUxrKG9BZjzFKXxDes1w",
-  authDomain: "tasklist-9b74e.firebaseapp.com",
-  projectId: "tasklist-9b74e",
-  storageBucket: "tasklist-9b74e.firebasestorage.app",
-  messagingSenderId: "816081176937",
-  appId: "1:816081176937:web:33a87cf382a317ec2f55f8"
+  apiKey: "AIzaSyBEt_GcMQa0MG2lBOUpIm18TnLIXTjFxWE",
+  authDomain: "metro-12243.firebaseapp.com",
+  projectId: "metro-12243",
+  storageBucket: "metro-12243.firebasestorage.app",
+  messagingSenderId: "1046779146859",
+  appId: "1:1046779146859:web:5792d58084e2afdbc2772e",
+  measurementId: "G-XEN821YR0M"
 );
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: firebaseConfig);
